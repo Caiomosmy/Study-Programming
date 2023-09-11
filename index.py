5b0edf709363fb6111cc1f8cd8c40aedbb56ee87
Caio Henrique - Full Stack Python <Caiomosmy@outlook.com>
2023-09-11 19:43:28 -0300

    Update index.py

--- index.py
+++ index.py
@@ -5,3 +5,5 @@ Ao criar um novo projeto Django, você precisa fazer algumas configurações ini
 2. **Instalar o Django:** Use o pip (gerenciador de pacotes do Python) para instalar o Django. Execute o seguinte comando no seu terminal:
 
 pip install django
+
+Criar um Novo Projeto Django: Use o comando django-admin ou manage.py para criar um novo projeto Django. Por exemplo:
