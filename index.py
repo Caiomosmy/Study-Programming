2d801ed0517578dcbd0af6400e6e3203cce0e981
Caio Henrique - Full Stack Python <Caiomosmy@outlook.com>
2023-09-11 19:42:59 -0300

    Update index.py

--- index.py
+++ index.py
@@ -1,2 +1,7 @@
 Configurações Iniciais do Projeto Django:
 Ao criar um novo projeto Django, você precisa fazer algumas configurações iniciais.
+
+1. **Criar um Ambiente Virtual (Opcional, mas recomendado):** É uma boa prática criar um ambiente virtual para isolar as dependências do seu projeto Django do sistema operacional. Você pode usar a ferramenta `virtualenv` para criar um ambiente virtual.
+2. **Instalar o Django:** Use o pip (gerenciador de pacotes do Python) para instalar o Django. Execute o seguinte comando no seu terminal:
+
+pip install django
