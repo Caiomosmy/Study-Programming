ce016b0f7009750d7081ea313499ac471b131a16
Caio Henrique - Full Stack Python <Caiomosmy@outlook.com>
2023-12-07 19:38:28 -0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,14 +1,14 @@
 /*ESTUDOS DE TYPESCRIPT*/
 
 /*INTRODUCÃO*/
-TypeScript é uma linguagem de programação estaticamente tipada que é um superconjunto de JavaScript. Foi desenvolvido e é mantido pela Microsoft. O TypeScript foi criado para enfrentar os desafios da criação de aplicativos JavaScript em larga escala e adiciona anotações de tipo opcionais, classes, interfaces e outros recursos à linguagem.
+/*TypeScript é uma linguagem de programação estaticamente tipada que é um superconjunto de JavaScript. Foi desenvolvido e é mantido pela Microsoft. O TypeScript foi criado para enfrentar os desafios da criação de aplicativos JavaScript em larga escala e adiciona anotações de tipo opcionais, classes, interfaces e outros recursos à linguagem.
 
 Os principais benefícios do uso do TypeScript incluem:
 
 Tipo Segurança
 Ferramentas Melhoradas
 Melhoria da Manutenção
-Compatibilidade Retrovertida
+Compatibilidade Retrovertida/*
 
 /*TYPESCRIPT TYPES*/
 
