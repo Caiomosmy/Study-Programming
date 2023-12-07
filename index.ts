a8857bdc324775cbe5633c03ad191dca8a57d940
Caio Henrique - Full Stack Python <Caiomosmy@outlook.com>
2023-12-07 19:54:08 -0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -137,4 +137,218 @@ Exclude: cria um tipo que é a diferença definida de A e B.
 ..e mais.
 Saiba mais nos seguintes links:*/
 
-/**/
+/* -- PARTIAL*/
+/*O tipo Parcial no TypeScript permite que você torne todas as propriedades de um tipo opcionais. 
+Isso é útil quando você precisa criar um objeto com apenas um subconjunto das propriedades de um tipo existente.
+
+Aqui está um exemplo de uso do tipo Parcial em TypeScript:*/
+interface User {
+  name: string;
+  age: number;
+  email: string;
+}
+
+function createUser(user: Partial<User>): User {
+  return {
+    name: 'John Doe',
+    age: 30,
+    email: 'john.doe@example.com',
+    ...user,
+  };
+}
+
+const newUser = createUser({ name: 'Jane Doe' });
+
+console.log(newUser);
+// Output: { name: 'Jane Doe', age: 30, email: 'john.doe@example.com' }
+
+/* -- PICK ()*/
+/*Pick constrói um tipo escolhendo o conjunto de propriedades Keys (string literal ou união de literais de string) de Type.*/
+interface Todo {
+  title: string;
+  description: string;
+  completed: boolean;
+}
+
+type TodoPreview = Pick<Todo, 'title' | 'completed'>;
+
+const todo: TodoPreview = {
+  title: 'Clean room',
+  completed: false,
+};
+
+/* -- OMIT*/
+/*Omit constrói um tipo escolhendo todas as propriedades de Type e, em seguida, removendo Keys 
+(string literal ou união de literais de string).*/
+
+interface Todo {
+  title: string;
+  description: string;
+  completed: boolean;
+  createdAt: number;
+}
+
+type TodoPreview = Omit<Todo, 'description'>;
+
+const todo: TodoPreview = {
+  title: 'Clean room',
+  completed: false,
+  createdAt: 1615544252770,
+};
+
+type TodoInfo = Omit<Todo, 'completed' | 'createdAt'>;
+
+const todoInfo: TodoInfo = {
+  title: 'Pick up kids',
+  description: 'Kindergarten closes at 5pm',
+};
+
+/* -- READONLY*/
+/*O Readonly constrói um tipo com todas as propriedades do tipo definidas como readonly, o que significa que as 
+propriedades do tipo construído não podem ser reatribuídas.*/
+
+interface Todo {
+  title: string;
+}
+
+const todo: Readonly<Todo> = {
+  title: 'Delete inactive users',
+};
+
+// Cannot assign to 'title' because it is a read-only property.
+todo.title = 'Hello';
+
+/* -- RECORD*/
+/*Record constrói um tipo de objeto cujas chaves de propriedade são Keys e cujos valores de propriedade são Type. 
+Este utilitário pode ser usado para mapear as propriedades de um tipo para outro tipo.*/
+
+interface CatInfo {
+  age: number;
+  breed: string;
+}
+
+type CatName = 'miffy' | 'boris' | 'mordred';
+
+const cats: Record<CatName, CatInfo> = {
+  miffy: { age: 10, breed: 'Persian' },
+  boris: { age: 5, breed: 'Maine Coon' },
+  mordred: { age: 16, breed: 'British Shorthair' },
+};
+
+/* -- EXCLUDE*/
+/*Excluir constrói um tipo excluindo do UnionType todos os membros do sindicato que são atribuíveis a ExcludedMembers.*/
+type T0 = Exclude<'a' | 'b' | 'c', 'a'>; // "b" | "c"
+type T1 = Exclude<'a' | 'b' | 'c', 'a' | 'b'>; // "c"
+type T2 = Exclude<string | number | (() => void), Function>; // string | number
+
+/* -- EXTRACT*/
+/*Extract constrói um tipo extraindo do Tipo todos os membros do sindicato que são atribuíveis ao Union.*/
+type T0 = Extract<'a' | 'b' | 'c', 'a' | 'f'>;
+//    ^ = type T0 = "a"
+
+/* -- NON NULLABLE*/
+/*Não-nulável constrói um tipo excluindo null e undefined do Tipo.*/
+type T0 = NonNullable<string | number | undefined>;
+// type T0 = string | number
+
+type T1 = NonNullable<string[] | null | undefined>;
+// type T1 = string[]
+
+/* -- PARAMETERS*/
+/*Parâmetros constrói um tipo de tupla dos tipos usados nos parâmetros de um tipo de função Tipo.*/
+type T0 = Parameters<() => string>;
+// type T0 = []
+
+type T1 = Parameters<(s: string) => void>;
+// type T1 = [s: string]
+
+type T2 = Parameters<<T>(arg: T) => T>;
+// type T2 = [arg: unknown]
+
+declare function f1(arg: { a: number; b: string }): void;
+type T3 = Parameters<typeof f1>;
+// type T3 = [arg: {
+//     a: number;
+//     b: string;
+// }]
+
+type T4 = Parameters<any>;
+// type T4 = unknown[]
+
+type T5 = Parameters<never>;
+// type T5 = never
+
+type T6 = Parameters<string>;
+// ^ Type 'string' does not satisfy the constraint '(...args: any) => any'.
+
+type T7 = Parameters<Function>;
+// ^ Type 'Function' does not satisfy the constraint '(...args: any) => any'.
+
+/* -- RETURN TYPE*/
+/*Tipo de retorno constrói um tipo que consiste no tipo de retorno da função Tipo.*/
+type T0 = ReturnType<() => string>;
+// type T0 = string
+
+type T1 = ReturnType<(s: string) => void>;
+// type T1 = void
+
+type T2 = ReturnType<<T>() => T>;
+// type T2 = unknown
+
+type T3 = ReturnType<<T extends U, U extends number[]>() => T>;
+// type T3 = number[]
+
+declare function f1(): { a: number; b: string };
+type T4 = ReturnType<typeof f1>;
+// type T4 = {
+//     a: number;
+//     b: string;
+// }
+
+type T5 = ReturnType<any>;
+// type T5 = any
+
+type T6 = ReturnType<never>;
+// type T6 = never
+
+type T7 = ReturnType<string>;
+// ^ Type 'string' does not satisfy the constraint '(...args: any) => any'.
+
+type T8 = ReturnType<Function>;
+// ^ Type 'Function' does not satisfy the constraint '(...args: any) => any'.
+
+/* -- INSTANCE TYPE*/
+/*Este tipo constrói um tipo que consiste no tipo de instância de uma função de construtor em Type.*/
+class C {
+  x = 0;
+  y = 0;
+}
+
+type T0 = InstanceType<typeof C>;
+// type T0 = C
+
+type T1 = InstanceType<any>;
+// type T1 = any
+
+type T2 = InstanceType<never>;
+// type T2 = never
+
+type T3 = InstanceType<string>;
+// ^ Type 'string' does not satisfy the constraint 'abstract new (...args: any) => any'.
+
+type T4 = InstanceType<Function>;
+// ^ Type 'Function' does not satisfy the constraint 'abstract new (...args: any) => any'.
+
+/* -- AWAITED*/
+/*Este tipo destina-se a modelar operações como await em funções assíncronas, ou o .then() método sobre Promessas -
+especificamente, a maneira como elas recursivamente desembrulham Promessas.*/
+type A = Awaited<Promise<string>>;
+// type A = string
+
+type B = Awaited<Promise<Promise<number>>>;
+// type B = number
+
+type C = Awaited<boolean | Promise<number>>;
+// type C = number | boolean
+
+
