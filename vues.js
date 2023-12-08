085513cadd3f615b6fe9ea2e3551eb2edd1acf7a
Caio Henrique - Full Stack Python <Caiomosmy@outlook.com>
2023-12-08 05:27:03 -0300

    Update vues.js

--- vues.js
+++ vues.js
@@ -1,3 +1,26 @@
+/*Tópicos Fundamentais*/
 
-Tópicos Fundamentais
-Vue é uma estrutura JavaScript para a construção de interfaces de usuário. Ele se baseia em HTML, CSS e JavaScript padrão e fornece um modelo de programação declarativa e baseada em componentes que ajuda você a desenvolver interfaces de usuário de forma eficiente, seja simples ou complexa.
+/*Vue é uma estrutura JavaScript para a construção de interfaces de usuário. Ele se baseia em HTML, CSS e JavaScript 
+padrão e fornece um modelo de programação declarativa e baseada em componentes que ajuda você a desenvolver 
+interfaces de usuário de forma eficiente, seja simples ou complexa.*/
+
+
+/*CLI Vues*/
+
+/*O Vue CLI é um sistema completo para o desenvolvimento rápido do Vue.js, fornecendo:
+
+- Andaimes interativos do projeto via @vue/cli.
+- Uma dependência de tempo de execução (@vue/cli-service) é isso:
+- Atualizável;
+- Construído em cima do webpack, com padrões sensatos;
+- Configurável através do arquivo de configuração no projeto;
+- Extensível através de plugins
+- Uma rica coleção de plugins oficiais que integram as melhores ferramentas no ecossistema frontend.
+- Uma interface gráfica completa para criar e gerenciar projetos Vue.js.
+
+O Vue CLI pretende ser a linha de base de ferramentas padrão para o ecossistema Vue. Ele garante que as várias 
+ferramentas de compilação funcionem sem problemas, juntamente com padrões sensatos, para que você possa se concentrar 
+em escrever seu aplicativo em vez de passar dias discutindo as configurações. Ao mesmo tempo, ainda oferece a 
+flexibilidade de ajustar a configuração de cada ferramenta sem a necessidade de ejeção.
+
+Visite os seguintes recursos para saber mais:
