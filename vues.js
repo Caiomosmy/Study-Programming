21778f55dbd0a573c25564b247e89d5ddda42bc5
Caio Henrique - Full Stack Python <Caiomosmy@outlook.com>
2023-12-08 05:24:58 -0300

    Update and rename index.vue to vues.js

---
+++ vues.js
@@ -0,0 +1,3 @@
+
+Tópicos Fundamentais
+Vue é uma estrutura JavaScript para a construção de interfaces de usuário. Ele se baseia em HTML, CSS e JavaScript padrão e fornece um modelo de programação declarativa e baseada em componentes que ajuda você a desenvolver interfaces de usuário de forma eficiente, seja simples ou complexa.
