dd1b120d45c5e1f849358cc47eb0128d97335839
Caio Henrique - Full Stack Python <Caiomosmy@outlook.com>
2023-12-29 09:55:57 -0300

    Update vues.js

--- vues.js
+++ vues.js
@@ -49,6 +49,20 @@ analisados por navegadores compatíveis com especificações e analisadores HTML
 /*No Vue.js, cada instância de aplicativo expõe um objeto config que contém configurações específicas para essa instância. Essas configurações podem ser modificadas antes de montar o aplicativo. 
 Isso oferece flexibilidade para personalizar o comportamento do Vue.js em nível de aplicativo. Vamos explorar alguns exemplos práticos:*/
 
+/*1. Modificação de Configurações Globais:*/
+
+javascript
+Copy code
+// main.js
+import { createApp } from 'vue';
+import App from './App.vue';
+
+const app = createApp(App);
+
+// Modificar a configuração global do aplicativo
+app.config.globalProperties.meuGlobal = 'Valor Global';
+
+app.mount('#app');
 
 /*---RENDERING LISTS*/
 
