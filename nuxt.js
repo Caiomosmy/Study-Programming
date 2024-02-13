2f19ca38c87947b08c34aafbea2c24e5ad2dfbad
Caio Henrique - Full Stack Python <Caiomosmy@outlook.com>
2024-02-13 13:34:14 -0300

    Atualizar o nuxt.js

--- nuxt.js
+++ nuxt.js
@@ -4,6 +4,19 @@
 Nuxt é uma estrutura gratuita e de código aberto com uma maneira intuitiva e extensível de criar aplicativos da web e sites full-stack com segurança de digitação, desempenho e nível de produção com Vue.js.
 Fizemos tudo para que você possa começar a escrever .vuearquivos desde o início enquanto aproveita a substituição de módulos em desenvolvimento e um aplicativo de alto desempenho em produção com renderização no servidor por padrão.
 Nuxt não depende de nenhum fornecedor, permitindo que você implante seu aplicativo em qualquer lugar, até mesmo na borda .*/
-//CONFIGURAÇÃO
+
+/*AUTOMAÇÃO E CONVERSÕES
+
+Nuxt usa convenções e uma estrutura de diretórios opinativa para automatizar tarefas repetitivas e permitir que os desenvolvedores se concentrem em enviar recursos. O arquivo de configuração ainda pode personalizar e substituir seus comportamentos padrão.
+Roteamento baseado em arquivo: defina rotas com base na estrutura do seu diretório . Isso pode facilitar a organização do seu aplicativo e evitar a necessidade de configuração manual de rotas.pages/
+Divisão de código: o Nuxt divide automaticamente seu código em partes menores, o que pode ajudar a reduzir o tempo de carregamento inicial do seu aplicativo.
+Renderização do lado do servidor pronta para uso: Nuxt vem com recursos SSR integrados, para que você não precise configurar um servidor separado.
+Importações automáticas: escreva elementos que podem ser compostos e componentes Vue em seus respectivos diretórios e use-os sem precisar importá-los com os benefícios da agitação da árvore e dos pacotes JS otimizados.
+Utilitários de busca de dados: o Nuxt fornece elementos que podem ser compostos para lidar com a busca de dados compatível com SSR, bem como diferentes estratégias.
+Suporte TypeScript de configuração zero: escreva código com segurança de tipo sem ter que aprender TypeScript com nossos tipos gerados automaticamente etsconfig.json
+Ferramentas de construção configuradas: usamos o Vite por padrão para oferecer suporte à substituição de módulo a quente (HMR) no desenvolvimento e agrupar seu código para produção com as melhores práticas incorporadas.
+Nuxt cuida disso e fornece funcionalidades de front-end e back-end para que você possa se concentrar no que importa: criar seu aplicativo da web .*/
+
+/*Renderização do lado do servidor*/
 
 //
