7efb6ea454bac625e381aba194315c5ac0feb680
Caio Henrique - Full Stack Python <Caiomosmy@outlook.com>
2024-02-12 21:32:43 -0300

    Atualizar o nuxt.js

--- nuxt.js
+++ nuxt.js
@@ -1,9 +1,9 @@
 //INTRODUÇÃO AO NUXT.JS 
 
-//O objetivo da Nuxt é tornar o desenvolvimento web intuitivo e de alto desempenho, com uma ótima experiência do desenvolvedor em mente.
+/*O objetivo da Nuxt é tornar o desenvolvimento web intuitivo e de alto desempenho, com uma ótima experiência do desenvolvedor em mente.
 Nuxt é uma estrutura gratuita e de código aberto com uma maneira intuitiva e extensível de criar aplicativos da web e sites full-stack com segurança de digitação, desempenho e nível de produção com Vue.js.
 Fizemos tudo para que você possa começar a escrever .vuearquivos desde o início enquanto aproveita a substituição de módulos em desenvolvimento e um aplicativo de alto desempenho em produção com renderização no servidor por padrão.
-Nuxt não depende de nenhum fornecedor, permitindo que você implante seu aplicativo em qualquer lugar, até mesmo na borda .//
+Nuxt não depende de nenhum fornecedor, permitindo que você implante seu aplicativo em qualquer lugar, até mesmo na borda .*/
 //CONFIGURAÇÃO
 
 //
