42e040f9797c01c2e1006e83f35e3d556c71518f
Caio Henrique - Full Stack Python <Caiomosmy@outlook.com>
2024-02-19 13:20:27 -0300

    Atualizar o index.js

--- index.js
+++ index.js
@@ -14,4 +14,13 @@
 /*EQUALITY COMPARISONS*/
 
 
-/*Asicrono*/
+/*ASSÍNCRONO*/
+A programação assíncrona é uma técnica que permite que seu programa inicie uma tarefa potencialmente de longa duração e ainda seja capaz de responder a outros eventos enquanto a tarefa é executada, em vez de ter que esperar até que a tarefa seja concluída. Assim que a tarefa for concluída, seu programa verá o resultado.
+
+Muitas funções fornecidas pelos navegadores, especialmente as mais interessantes, podem potencialmente demorar muito e, portanto, são assíncronas. Por exemplo:
+
+Fazendo solicitações HTTP usandofetch()
+Acessando a câmera ou microfone de um usuário usandogetUserMedia()
+Pedir a um usuário para selecionar arquivos usandoshowOpenFilePicker()
+Portanto, mesmo que você não precise implementar suas próprias funções assíncronas com muita frequência, é muito provável que você precise usá-las corretamente.
+
